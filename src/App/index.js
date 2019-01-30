664ba90c02fdeb735c985ed4ce3f6435775e7f0d
Shaun Dawson <shaundawson0883@gmail.com>
2019-01-30 14:10:12 -0800

    created navbar component

--- src/App/index.js
+++ src/App/index.js
@@ -1,24 +1,12 @@
 import React, { Component } from "react";
 import "./style.css";
+import Navbar from "../Navbar";
+
 
 class App extends Component {
   render() {
-    return <ul class="nav">
-      <li class="nav-item">
-        <a class="nav-link active" href="#">Active</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>;
+    return <Navbar/>;
   }
 }
 
 export default App;
-
