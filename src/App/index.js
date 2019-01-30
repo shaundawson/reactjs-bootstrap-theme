d6d92379e345d99d1caba796e0a27d149970e943
Shaun Dawson <shaundawson0883@gmail.com>
2019-01-30 13:47:27 -0800

    install bootstrap

--- src/App/index.js
+++ src/App/index.js
@@ -3,8 +3,22 @@ import "./style.css";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return <ul class="nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>;
   }
 }
 
 export default App;
+
